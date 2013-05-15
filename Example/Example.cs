2e0a8ee72e44b4590c8cd335ed4d1388aeaf7a33
renmengye <renmengye@gmail.com>
2013-05-15 04:14:28 -0400

    first commit

---
+++ Example/Example.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Base62.Example
+{
+    class Example
+    {
+        void Encode()
+        {
+            string s = (new byte[] { 116, 32, 8, 99, 100, 232, 4, 7 }).ToBase62();
+        }
+
+        void Decode()
+        {
+            byte[] b = "T208OsJe107".FromBase62();
+        }
+    }
+}
