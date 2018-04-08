081d3be3ec7bcf6583929d1c20ea84b025d8317c
Chino Chang <chino@joymoe.com>
2018-04-08 23:41:39 +0800

    Upgrade the projects
    * [x] Use the new format of project files
    * [x] Use xUnit for unit testing
    * [x] Pack the nupkg file while building

---
+++ tests/Base62.Tests/BasicTests.cs
@@ -0,0 +1,17 @@
+using System.Linq;
+using Xunit;
+
+namespace Base62.Tests
+{
+    public class BasicTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            byte[] x = { 128, 128, 128, 128, 128, 128 };
+            string s = x.ToBase62();
+            byte[] x2 = s.FromBase62();
+            Assert.True(x.SequenceEqual(x2));
+        }
+    }
+}
